9d3208d6569522f316b653a393cff7bf8b11d4f7
jhicks2306 <jhicks2306@gmail.com>
2024-01-25 17:56:10 +0000

    Add folium map with markers

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,54 +1,26 @@
 from pathlib import Path
-
 import pandas as pd
-import seaborn as sns
-from ipyleaflet import GeoJSON, Map
 from shinywidgets import output_widget, register_widget
 import folium
 
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
-sns.set_theme(style="white")
-df = pd.read_csv(Path(__file__).parent / "penguins.csv", na_values="NA")
-df_resorts = pd.read_csv("ski-resorts.csv")
-species = ["Adelie", "Gentoo", "Chinstrap"]
-
-
-def make_value_box(penguin):
-    return ui.value_box(
-        title=penguin, value=ui.output_text(f"{penguin}_count".lower()), theme="primary"
-    )
+df = pd.read_csv("ski-resorts.csv")
 
 
 app_ui = ui.page_sidebar(
     ui.sidebar(
         ui.input_slider(
-            "mass",
-            "Mass",
-            2000,
-            6000,
-            3400,
+            'top_snow',
+            'Snow on top (cm)',
+            0,
+            200,
+            50,
         ),
-        ui.input_checkbox_group(
-            "species", "Filter by species", species, selected=species
-        ),
-    ),
-    ui.row(
-        ui.layout_columns(
-            *[make_value_box(penguin) for penguin in species],
-        )
-    ),
-    ui.row(
-        ui.layout_columns(
-            ui.card(
-                ui.card_header("Summary statistics"),
-                ui.output_data_frame("summary_statistics"),
-            ),
-            ui.card(
-                ui.card_header("Penguin bills"),
-                ui.output_plot("length_depth"),
-            ),
-        ),
+        ui.input_switch(
+            'toggle',
+            'Hide closed resorts',
+            value=False)
     ),
     ui.row(
         ui.card(
@@ -56,79 +28,47 @@ app_ui = ui.page_sidebar(
             ui.output_ui('folium_map')
         )          
     ),
-    ui.row(
-        ui.card(
-            ui.card_header("Resorts"),
-            ui.output_data_frame("resort_summary"),
-        ),         
-    ),
 )
 
-
 def server(input: Inputs, output: Outputs, session: Session):
+    
+    # Apply filters to DataFrame
     @reactive.Calc
-    def filtered_df() -> pd.DataFrame:
-        filt_df = df[df["Species"].isin(input.species())]
-        filt_df = filt_df.loc[filt_df["Body Mass (g)"] > input.mass()]
+    def filtered_df():
+        filt_df = df
+        filt_df = filt_df.loc[filt_df['Snow on Top (cm)'] > input.top_snow()]
         return filt_df
-    
 
-    @render.text
-    def adelie_count():
-        return count_species(filtered_df(), "Adelie")
-
-    @render.text
-    def chinstrap_count():
-        return count_species(filtered_df(), "Chinstrap")
-
-    @render.text
-    def gentoo_count():
-        return count_species(filtered_df(), "Gentoo")
-
-    @render.plot
-    def length_depth():
-        return sns.scatterplot(
-            data=filtered_df(),
-            x="Bill Length (mm)",
-            y="Bill Depth (mm)",
-            hue="Species",
-        )
-
-    @render.data_frame
-    def summary_statistics():
-        display_df = filtered_df()[
-            [
-                "Species",
-                "Island",
-                "Bill Length (mm)",
-                "Bill Depth (mm)",
-                "Body Mass (g)",
-            ]
-        ]
-        return render.DataGrid(display_df, filters=True)
-    
-    @render.data_frame
-    def resort_summary():
-        return render.DataGrid(df_resorts, width='100%')
-    
-
+    # Render map.
     @render.ui
     def folium_map():
-        return build_resort_map(df_resorts)
+        display_df = filtered_df()
+        return build_resort_map(display_df)
 
-
-def count_species(df, species):
-    return df[df["Species"] == species].shape[0]
 
 def build_resort_map(df):
+    """
+    Builds map of Ski resorts with markers.
+    """
+    
+    # Create map and center.
     avg_lat = df['Latitude'].mean()
     avg_lon = df['Longitude'].mean()
     map = folium.Map(location=[avg_lat, avg_lon], zoom_start=6)
 
     for index, row in df.iterrows():
+
+        # Show green if resort is open.
+        if row['Resort Open'] == True:
+            color = 'green'
+        else:
+            color = 'red'
+
+        # Add markers.
         folium.Marker(
             location=[row['Latitude'], row['Longitude']],
-            popup=row['Name']
+            popup=row['Name'],
+            icon=folium.Icon(color=color)
         ).add_to(map)
 
     return map
